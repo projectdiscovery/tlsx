a071c7693f1ae06973ac79bcb5b38b00ed06ed23
sandeep <8293321+ehsandeep@users.noreply.github.com>
2023-08-10 22:02:17 +0530

    version update

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -18,7 +18,7 @@ var banner = fmt.Sprintf(`
    |_| |____|___/_/\_\	%s
 `, version)
 
-const version = "v1.1.2"
+const version = "v1.1.3"
 
 // validateOptions validates the provided options for crawler
 func (r *Runner) validateOptions() error {
