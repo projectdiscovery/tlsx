3f068da6f75b423f9eeaf77e11f61d39511c5387
Solari Systems <solarisys2025@gmail.com>
2026-02-23 08:31:50 -0500

    fix: tlsx hangs indefinitely for some hosts
    Addresses #819
    
    Automated fix by Solari

--- pkg/tlsx/tls/tls.go
+++ pkg/tlsx/tls/tls.go
@@ -236,10 +236,16 @@ func (c *Client) EnumerateCiphers(hostname, ip, port string, options clients.Con
 
 		conn := tls.Client(baseConn, baseCfg)
 
-		if err := conn.Handshake(); err == nil {
+		timeout := time.Duration(c.options.Timeout) * time.Second
+		if timeout == 0 {
+			timeout = 5 * time.Second
+		}
+		enumCtx, enumCancel := context.WithTimeout(context.Background(), timeout)
+		if err := conn.HandshakeContext(enumCtx); err == nil {
 			ciphersuite := conn.ConnectionState().CipherSuite
 			enumeratedCiphers = append(enumeratedCiphers, tls.CipherSuiteName(ciphersuite))
 		}
+		enumCancel()
 		_ = conn.Close() // close baseConn internally
 	}
 	return enumeratedCiphers, nil
